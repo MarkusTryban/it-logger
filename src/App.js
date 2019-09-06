20d6a925de9a360386d113cfd328d6b56cc89266
MarkusTryban <markustryban@gmail.com>
2019-09-06 19:24:31 -0400

    add materialize-css, useEffect & AutoInit to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css'
 
 const App = () => {
+  useEffect(() => {
+    // Init materialize JS
+    M.AutoInit()
+  })
   return (
     <div className='App'>
       <h1>Hello World</h1>
