57ccee546bd8e1ff68ec05d815d0850aa6655d5a
MarkusTryban <markustryban@gmail.com>
2019-09-09 15:29:00 -0400

    add AddBtn.js

---
+++ src/components/layout/AddBtn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddBtn = () => {
+  return (
+    <div>
+      <a
+        href='#add-log-modal'
+        className='btn-floating btn-larg blue darken-2 modal-trigger'
+      >
+        <i className='large material-icons'>add</i>
+      </a>
+    </div>
+  )
+}
+
+export default AddBtn
