60cb4d82f4fa935724a178da3c59797e16591e98
MarkusTryban <markustryban@gmail.com>
2019-09-17 16:19:49 -0400

    add TechListModal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddBtn from './components/layout/AddBtn'
 import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from './components/techs/AddTechModal'
+import TechsListModal from './components/techs/TechListModal'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
@@ -23,6 +24,7 @@ const App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechsListModal />
         <Logs />
       </div>
     </Fragment>
