13442af1ad4b35642b49f53ebc10551a2040981f
MarkusTryban <markustryban@gmail.com>
2019-09-19 16:35:34 -0400

    add case for LOGS_ERROR

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -14,6 +14,11 @@ export default (state = initialState, action) => {
         ...state,
         loading: true
       }
+    case LOGS_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      }
     default:
       return state
   }
