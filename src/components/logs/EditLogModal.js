8f9ec403ec1da02928835bc374e776d58595451f
MarkusTryban <markustryban@gmail.com>
2019-09-21 19:43:43 -0400

    add connect & updateLog to EditLogModal

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import M from 'materialize-css/dist/js/materialize.min.js'
+import { updateLog } from '../../actions/logActions'
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('')
   const [attention, setAttention] = useState(false)
   const [tech, setTech] = useState('')
@@ -88,4 +91,16 @@ const modalStyle = {
   height: '75%'
 }
 
-export default EditLogModal
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  current: state.log.current
+})
+
+export default connect(
+  mapStateToProps,
+  { updateLog }
+)(EditLogModal)
