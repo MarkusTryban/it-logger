311771017747df853ffdd5a8a9f016af1d6db93d
MarkusTryban <markustryban@gmail.com>
2019-09-24 15:20:56 -0400

    add useEffect to TechSelectOptions

--- src/components/techs/TechSelectOptions.js
+++ src/components/techs/TechSelectOptions.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { getTechs } from '../../actions/techActions'
 
 const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
+  useEffect(() => {
+    getTechs()
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div>
-      <h4>Tech</h4>
-    </div>
+    !loading &&
+    techs !== null &&
+    techs.map(t => (
+      <option key={t.id} value={`${t.firstName} ${t.lastName}`}>
+        {t.firstName} {t.lastName}
+      </option>
+    ))
   )
 }
 
