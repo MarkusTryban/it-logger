25edcc924fbb2a792f790a3618fc5497b9a62072
MarkusTryban <markustryban@gmail.com>
2019-12-05 16:46:39 -0500

    Update logs & techs routes

--- routes/logs.js
+++ routes/logs.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const config = require('config');
+
+const Log = require('../models/log');
 
 router.post('/', (req, res) => {
   res.send('Add a log');
--- routes/techs.js
+++ routes/techs.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const config = require('config');
+
+const Tech = require('../models/tech');
 
 router.post('/', (req, res) => {
   res.send('Add a tech');
