6ad5899d8f5dd19c36236cb71ab500d876d095c8
MarkusTryban <markustryban@gmail.com>
2019-12-04 20:01:17 -0500

    Add routes for logs & techs

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World'));
 
+app.use('/api/logs', require('./routes/logs'));
+app.use('/api/techs', require('./routes/techs'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
