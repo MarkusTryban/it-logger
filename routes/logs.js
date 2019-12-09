e8cd8f96546eee3d34497c1ca1451e8746621526
MarkusTryban <markustryban@gmail.com>
2019-12-08 19:49:16 -0500

    Add route POST /api/logs

--- routes/logs.js
+++ routes/logs.js
@@ -13,4 +13,34 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post(
+  '/',
+  [
+    check('message', 'message is required.')
+      .not()
+      .isEmpty(),
+    check('tech', 'tech is required.')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    try {
+      const { message, tech, attention } = req.body;
+
+      const log = new Log({
+        message,
+        tech,
+        attention
+      });
+
+      await log.save();
+
+      res.json(log);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error.');
+    }
+  }
+);
+
 module.exports = router;
