5ec92d59f2ffc3decc32bc4f080ed148db8bfb73
MarkusTryban <markustryban@gmail.com>
2019-12-08 20:08:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(express.json({ extended: false }));
 
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/techs', require('./routes/techs'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
