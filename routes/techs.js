c9e617dc74f224ef585347fa944edc98cd2f0648
MarkusTryban <markustryban@gmail.com>
2019-12-08 20:01:48 -0500

    Add POST /api/techs

--- routes/techs.js
+++ routes/techs.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const router = express.Router();
 const Tech = require('../models/Tech');
@@ -12,5 +13,34 @@ router.get('/', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+router.post(
+  '/',
+  [
+    check('firstName', 'First Name is required.')
+      .not()
+      .isEmpty(),
+    check('lastName', 'Last Name is required.')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    try {
+      const { firstName, lastName } = req.body;
+
+      const tech = new Tech({
+        firstName,
+        lastName
+      });
+
+      await tech.save();
+
+      res.json(tech);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error.');
+    }
+  }
+);
 
 module.exports = router;
