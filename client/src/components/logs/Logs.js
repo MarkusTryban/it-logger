7923bfce2187bd6e8b4845f138b95f71eba24797
MarkusTryban <markustryban@gmail.com>
2019-12-16 19:31:48 -0500

    Formatting & cleanup

--- client/src/components/logs/Logs.js
+++ client/src/components/logs/Logs.js
@@ -13,19 +13,19 @@ const Logs = ({ log: { logs, loading, search }, getLogs }) => {
   }, []);
 
   if (loading || logs === null) {
-    return <Preloader />
-  };
+    return <Preloader />;
+  }
 
   return (
     <ul className='collection with-header'>
       <li className='collection-header'>
-        <h4 className='center'>Sytem Logs</h4>
+        <h4 className='center'>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className='center'>No logs to show...</p>
+      ) : search !== null ? (
+        search.map(log => <LogItem key={log._id} log={log} />)
       ) : (
-        search !== null ?
-        search.map(log => <LogItem key={log._id} log={log} />) :
         logs.map(log => <LogItem key={log._id} log={log} />)
       )}
     </ul>
